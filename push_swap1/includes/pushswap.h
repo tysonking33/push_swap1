d117215c8d85fbdb4ccda31467bbe3ae83a8cbf9
Tyson Tang <90588335+tysonking33@users.noreply.github.com>
2022-09-19 18:11:24 +0930

    Add files via upload

--- push_swap1/includes/pushswap.h
+++ push_swap1/includes/pushswap.h
@@ -13,6 +13,7 @@ typedef struct s_arrays
 	int	b_size;
 	int step_count;
 	int	*bubble_sort_arr;
+	int	*b_moves;
 }			t_arrays;
 
 void	pa(t_arrays *array);
@@ -26,13 +27,14 @@ void	print_array(t_arrays *array, char *move_type);
 void    ft_ibzero(t_arrays *array);
 void    rotateb(t_arrays *array);
 void    revrotatea(t_arrays *array);
+void    revrotateb(t_arrays *array);
 void	swapa(t_arrays *array);
 void	swapb(t_arrays *array);
 void	sim_swap(t_arrays *array);
 void    small_med_large(t_arrays *array);
 int		*bubble_sort(t_arrays *array);
 void    large_sort(t_arrays *array);
-int check_sorted(t_arrays *array);
-void btop_more_than_abot(t_arrays *array);
-void btop_less_than_abot(t_arrays *array);
+int		check_sorted(t_arrays *array);
+void	btop_more_than_abot(t_arrays *array);
+void	btop_less_than_abot(t_arrays *array);
 #endif
--- push_swap1/sources/large_sort.c
+++ push_swap1/sources/large_sort.c
@@ -76,6 +76,104 @@ void btop_less_than_abot(t_arrays *array)
 		rotatea(array);
 }
 
+void	push_b_to_a(t_arrays *array, int rot_num)
+{
+	int ctr = 0;
+	if (rot_num != 0)
+	{
+		if (rot_num >= array->b_size / 2)
+		{
+			while (ctr < array->b_size - rot_num + 1)
+			{
+				revrotateb(array);
+				ctr++;
+			}
+		}
+		else
+		{
+			while (ctr < rot_num)
+			{
+				rotateb(array);
+				ctr++;
+			}
+		}
+	}
+	else
+		revrotateb(array);
+	pa(array);
+	rotatea(array);
+}
+
+//finding which combinationof a and b elements that has the lowest number of steps
+void	find_min_moves_arr(t_arrays *array)
+{
+	int move_ctr;
+	int b_ele;					//the index of array b
+
+	b_ele = array->b_size;
+	//finding bottom of a_element in bubble_sort array
+	int a_loc_in_bb;
+	a_loc_in_bb = 0;
+	while (array->a[0] != array->bubble_sort_arr[a_loc_in_bb])
+		a_loc_in_bb++;
+
+	//finding the closet b_element, relative to the bottom of a in bubble sort arr
+	while (b_ele >= 0)
+	{
+		move_ctr = 0;
+		while (array->b[b_ele] != array->bubble_sort_arr[move_ctr])			//finding b in bb_sort
+			move_ctr++;
+		if (a_loc_in_bb > move_ctr)
+			move_ctr = a_loc_in_bb - move_ctr;
+		else
+			move_ctr = move_ctr - a_loc_in_bb;
+		array->b_moves[b_ele] = move_ctr;
+		b_ele--;
+	}
+	/*printf("\n----------------------------------------------------------------------------------------\n");
+	for (int i = array->b_size; i >= 0;i--)
+	{
+		printf("b_move[%d]: %d\n", i, array->b_moves[i]);
+	}
+	printf("\n----------------------------------------------------------------------------------------\n");*/
+}
+
+
+int	find_min_moves(t_arrays *array)
+{
+	int curr_min;
+	int act_min;
+	int b_ele;
+	int ctr;
+
+	curr_min = array->b_moves[0];
+	act_min = 0;
+	b_ele = array->b_size;
+	ctr = 0;
+
+	while (ctr < b_ele)
+	{
+		if (curr_min > array->b_moves[ctr])
+			act_min = ctr;
+		ctr++;
+	}
+
+	printf("\nactual_min indx: %d\n", act_min);
+	return act_min;
+}
+
+void b_move_empty(t_arrays *array)
+{
+	int ctr;
+	ctr = 0;
+	while (ctr < array->b_size)
+	{
+		array->b_moves[ctr] = 0;
+		ctr++;
+	}
+}
+
+
 void	large_sort(t_arrays *array)
 {
 	//finding location of smallest number
@@ -116,24 +214,30 @@ void	large_sort(t_arrays *array)
 		/*while (array->a[ele_loc] != array->bubble_sort_arr[bb_loc])
 			ele_loc++;
 		printf("bb_loc: %d, ele_loc: %d\n", bb_loc, ele_loc);*/
-		if ((array->a[array->a_size] >= array->bubble_sort_arr[3*array_a_size/4]) && (array->a[array->a_size - 1] == array->bubble_sort_arr[0]))
-			rotatea(array);
-		else if ((array->a[array->a_size] > array->a[0]) && (bb_loc > (3*array->a_size / 4)))
+		/*if ((array->a[array->a_size] >= array->bubble_sort_arr[3*array_a_size/4]) && (array->a[array->a_size - 1] <= array->bubble_sort_arr[(array->a_size)/4]))
+			pb(array);
+		else */if ((array->a[array->a_size] > array->a[0]) && (bb_loc > (3*(array->a_size) / 4)))
 			rotatea(array);
 		else 
 			pb(array);
 		//else
 		//	rotatea(array);
 	}
+	array->b_moves = (int *)malloc((array->b_size)*sizeof(int *));
 	printf("*********************************************************************************************\n");
 	//int array_b_size = array->b_size;
-	while (check_sorted(array) != 1 && array->b_size >= 0)
+	/*while (check_sorted(array) != 1 && array->b_size >= 0)
 	{
 		if (array->b[array->b_size] > array->a[0] && array->b_size >= 0)
 			btop_more_than_abot(array);
 		else if(array->b[array->b_size] < array->a[0] && array->b_size >= 0)
 			btop_less_than_abot(array);
+	}*/
+	while (array->b_size >= 0)
+	{
+		find_min_moves_arr(array);
+		printf("find min moves %d\n", find_min_moves(array));
+		push_b_to_a(array, find_min_moves(array));
+		b_move_empty(array);
 	}
-		
-
 }
